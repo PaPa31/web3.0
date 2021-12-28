03b0aaaa825dd439e07f86950b4827848b19f1ca
papa31 <visidoor@yandex.ru>
2021-12-29 01:54:50 +0500

    add navbar for mobile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import { default as React } from "react";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiMenuAlt4 } from "react-icons/hi";
 import logo from "../../images/logo.png";
 
 const NavbarItem = ({ title, classProps }) => {
@@ -6,6 +8,7 @@ const NavbarItem = ({ title, classProps }) => {
 };
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -19,6 +22,42 @@ const Navbar = () => {
           Login
         </li>
       </ul>
+      <div className="flex relative">
+        {toggleMenu ? (
+          <AiOutlineClose
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <HiMenuAlt4
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <ul
+            className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+          flex flex-col justify-start items-end rounded-md
+          blue-glassmorphism text-white animate-slide-in
+          "
+          >
+            <li className="text-xl w-full my-2 ">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+              (item, index) => (
+                <NavbarItem
+                  key={item + index}
+                  title={item}
+                  classProps="my-2 text-lg"
+                />
+              )
+            )}
+          </ul>
+        )}
+      </div>
     </nav>
   );
 };
